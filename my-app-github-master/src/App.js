22c3f42c5cf23edb42aa61db2ad6fcd007f12ddc
DESKTOP-6U1869F\Asus <kajal.jaypatil@ojassoft.in>
2022-07-26 19:29:20 +0530

    Add usestate and useeffect in react

--- my-app-github-master/src/App.js
+++ my-app-github-master/src/App.js
@@ -3,6 +3,7 @@ import FirstComp from "./Component/FirstComp";
 import SecComp from "./Component/SecComp";
 import Header from "./Header";
 import Content from "./Component/Content";
+import Counter from "./MyComponents/Counter";
 
 // props, state
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <div className="App">
       <Header name="Jay" age="28" city="Pune" />
       <Header name="kajal" age="20" city="Pune" />
+      <Counter />
       <Content name="Kajal Jaykumar Patil" />
       <FirstComp />
       <SecComp />
---
+++ my-app-github-master/src/MyComponents/Counter.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react"
+import './MyComponents.css'
+
+const Counter = () => {
+
+    const [count, setCount] = useState(1);
+
+    function add() {
+        setCount(count + 1)
+    }
+
+    function minus() {
+        setCount(count - 1)
+    }
+
+    useEffect(() => {
+        alert('HI Kajal')
+    }, [])
+
+    useEffect(() => {
+        alert(`count change ${count} zala re bho!!`)
+    },[count])
+
+
+    return (
+        <div className="container">
+            <h2>{count}</h2>
+            <button onClick={add}>+</button>
+            <button onClick={minus}>-</button>
+        </div>
+    )
+}
+
+export default Counter
